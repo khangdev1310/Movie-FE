e3c0cd3b78085ba5d42bf2258e3909f7122ae683
Trong Binh <trongbinhnguyen15@gmail.com>
2022-07-02 09:28:33 +0700

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { FC } from "react";
 import "./App.css";
 const App: FC = () => {
   return (
-    <div className="App">
-      <p className="text-yellow-500 text-center">Hello world</p>
+    <div className="flex flex-col h-screen gap-1">
+      <div className="p-4 bg-red-200 h-[100px]">Header</div>
+      <div className="flex flex-col h-full gap-1">
+        <div className="p-4 bg-blue-200 h-[100px]">Table of Content</div>
+        <div className="h-full p-4 bg-yellow-200">Content</div>
+      </div>
+      <div className="p-4 bg-green-200 h-[100px]">Footer</div>
     </div>
   );
 };
