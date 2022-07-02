6874f481d666c6f2f03b6b7ffba11b83dbd7dd3b
Trong Binh <trongbinhnguyen15@gmail.com>
2022-07-02 09:34:20 +0700

    tablet

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ const App: FC = () => {
   return (
     <div className="flex flex-col h-screen gap-1">
       <div className="p-4 bg-red-200 h-[100px]">Header</div>
-      <div className="flex flex-col h-full gap-1">
-        <div className="p-4 bg-blue-200 h-[100px]">Table of Content</div>
-        <div className="h-full p-4 bg-yellow-200">Content</div>
+      <div className="flex flex-col h-full gap-1 md:flex-row">
+        <div className="p-4 bg-blue-200 h-[100px] md:h-full">Table of Content</div>
+        <div className="h-full p-4 bg-yellow-200 md:w-full">Content</div>
       </div>
       <div className="p-4 bg-green-200 h-[100px]">Footer</div>
     </div>
