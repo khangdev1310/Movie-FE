c7bd8051adc192e5fc1ceb09d7d9ec1b5a52f682
Trong Binh <trongbinhnguyen15@gmail.com>
2022-07-02 09:00:21 +0700

    laptop

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ const App: FC = () => {
     <div className="App">
       <div className="p-4 bg-red-200 header">Header</div>
       <div className="p-4 bg-blue-200 menu">Menu</div>
-      <div className="p-4 bg-yellow-200 content">Content</div>
+      <div className="p-4 bg-yellow-200 content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis labore, est quidem suscipit, dolor iste cupiditate sed beatae aperiam molestias, in iure corporis ullam explicabo soluta quo ab expedita ea.
+      Quo dolore ullam fuga soluta reiciendis doloribus delectus praesentium pariatur aspernatur, exercitationem voluptatum, facilis ratione sed laudantium quidem consequuntur nemo dolorum fugit, optio et itaque? Reiciendis accusantium rem quo hic?
+      Quasi omnis alias vitae! Aliquam itaque maxime ut dolore, velit sed voluptate et, dicta odit aspernatur veritatis quia exercitationem consequuntur! Doloribus animi facere dignissimos, magni adipisci beatae molestias quas facilis!
+      Maxime error repellendus nesciunt minus repellat praesentium neque voluptates impedit ex veniam, ratione vel, est consectetur alias possimus facere quo corporis, perferendis esse placeat! Consectetur fugiat eligendi totam ab eos.</div>
       <div className="p-4 bg-green-200 footer">Footer</div>
     </div>
   );
