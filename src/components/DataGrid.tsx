9439347f694d3a58e92d1133222ae0b81b42f15b
Trong Binh <trongbinhnguyen15@gmail.com>
2022-07-29 22:16:58 +0700

    feat: add husky commitlint eslint prettier

--- src/components/DataGrid.tsx
+++ src/components/DataGrid.tsx
@@ -1,20 +1,20 @@
-import React, { FC } from "react";
-import { FaPlay } from "react-icons/fa";
+import { FC } from 'react';
+import { FaPlay } from 'react-icons/fa';
 
 type DataType = {
-    id: string;
-    image: string;
-    title: string;
-    description?: string;
-  };
+  id: string;
+  image: string;
+  title: string;
+  description?: string;
+};
 
 interface DataGridProps {
   data: DataType[];
-  type: "link" | "button";
+  type: 'link' | 'button';
 }
 
 const GridItem: FC<DataType> = (item) => (
-  <div key={item.id} className='p-2 bg-gray-800 group'>
+  <div key={item.id} className="p-2 bg-gray-800 group">
     <div className="w-full relative h-0 pb-[100%]  cursor-pointer">
       <img
         src={item.image}
@@ -22,9 +22,7 @@ const GridItem: FC<DataType> = (item) => (
         className="absolute h-full w-full object-cover rounded-md group-hover:brightness-[80%] transition duration-300"
       />
 
-      <div
-        className="absolute flex items-center justify-center w-10 h-10 transition duration-300 -translate-x-1/2 -translate-y-1/2 border rounded-full opacity-0 top-1/2 left-1/2 group-hover:opacity-100 "
-      >
+      <div className="absolute flex items-center justify-center w-10 h-10 transition duration-300 -translate-x-1/2 -translate-y-1/2 border rounded-full opacity-0 top-1/2 left-1/2 group-hover:opacity-100 ">
         <FaPlay className="w-5 h-5 fill-white" />
       </div>
     </div>
@@ -36,12 +34,12 @@ const GridItem: FC<DataType> = (item) => (
       <p className="text-gray-400 line-clamp-2">{item.description} </p>
     )}
   </div>
- )
+);
 
 const DataGrid: FC<DataGridProps> = ({ data }) => {
   return (
     <div className="grid gap-3 grid-cols-fill-small md:grid-cols-fill-medium">
-      {data.map((item) =>  GridItem(item))}
+      {data.map((item) => GridItem(item))}
     </div>
   );
 };
